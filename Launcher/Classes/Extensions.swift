d3f3b3c285e4122e55a1eb9c313d4c3e3acece7c
Bas Broek <bas@basbroek.nl>
2017-10-20 16:57:08 +0200

    Simplify filteredRegexMatches

--- Launcher/Classes/Extensions.swift
+++ Launcher/Classes/Extensions.swift
@@ -38,13 +38,9 @@ extension String {
     }
 
     func filteredRegexMatches(pattern: String, replaceWith replacement: String = "") -> String {
-        do {
-            let regex = try NSRegularExpression(pattern: pattern, options: .caseInsensitive)
-            let range = NSRange(location: 0, length: count)
-            return regex.stringByReplacingMatches(in: self, range: range, withTemplate: replacement)
-        } catch {
-            return self
-        }
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive) else { return self }
+        let range = NSRange(location: 0, length: count)
+        return regex.stringByReplacingMatches(in: self, range: range, withTemplate: replacement)
     }
 }
 
