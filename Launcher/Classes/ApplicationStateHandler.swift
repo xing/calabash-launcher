49c0439d46f67ed54843e4ab1fbc5648388da80e
Bas Broek <bas@basbroek.nl>
2017-10-27 11:30:28 +0200

    Make the state handler less brittle by using enums; always return raw value

--- Launcher/Classes/ApplicationStateHandler.swift
+++ Launcher/Classes/ApplicationStateHandler.swift
@@ -3,8 +3,22 @@ import AppKit
 class ApplicationStateHandler {
     
     private let defaults = UserDefaults.standard
+
+    fileprivate enum Keys: String {
+        case simulatorRadioButtonState = "simRadioButton"
+        case physicalRadioButtonState = "phyRadioButton"
+        case isLaunched = "wasLaunched"
+        case buildNumber = "buildNumber"
+        case filePath = "FilePath"
+        case phoneName = "PhoneName"
+        case phoneUDID = "PhoneUDID"
+        case language = "Language"
+        case tag = "Tag"
+        case debugState = "DebugCheckbox"
+        case cucumberProfile = "cucumberProfile"
+    }
     
-    var simulatorRadioButtonState: Int? {
+    var simulatorRadioButtonState: Int {
         get {
             return defaults.integer(forKey: "simRadioButton")
         }
@@ -13,9 +27,9 @@ class ApplicationStateHandler {
         }
     }
     
-    var physicalButtonState: NSControl.StateValue? {
+    var physicalButtonState: Int {
         get {
-            return NSControl.StateValue(rawValue: defaults.integer(forKey: "phyRadioButton"))
+            return defaults.integer(forKey: "phyRadioButton")
         }
         set {
             defaults.set(newValue, forKey:"phyRadioButton")
@@ -25,82 +39,104 @@ class ApplicationStateHandler {
     // todo: does this really need to be handled in the application state handler? it's only used in the view controller
     var isLaunched: Bool {
         get {
-            return defaults.bool(forKey: "wasLaunched")
+            return defaults.bool(forKey: .isLaunched)
         }
         set {
-            defaults.setValue(newValue, forKey: "wasLaunched")
+            defaults.set(newValue, forKey: .isLaunched)
         }
     }
     
     var buildNumber: Int {
         get {
-            return defaults.integer(forKey: "buildNumber")
+            return defaults.integer(forKey: .buildNumber)
         }
         set {
-            defaults.set(newValue, forKey: "buildNumber")
+            defaults.set(newValue, forKey: .buildNumber)
         }
     }
     
     var filePath: URL? {
         get {
-            return defaults.url(forKey: "FilePath")
+            return defaults.url(forKey: .filePath)
         }
         set {
-            defaults.set(newValue, forKey:"FilePath")
+            defaults.set(newValue, forKey: .filePath)
         }
     }
     
     var phoneName: String? {
         get {
-            return defaults.string(forKey: "PhoneName")
+            return defaults.string(forKey: .phoneName)
         }
         set {
-            defaults.set(newValue, forKey:"PhoneName")
+            defaults.set(newValue, forKey: .phoneName)
         }
     }
     
     var phoneUDID: String? {
         get {
-            return defaults.string(forKey: "PhoneUDID")
+            return defaults.string(forKey: .phoneUDID)
         }
         set {
-            defaults.set(newValue, forKey:"PhoneUDID")
+            defaults.set(newValue, forKey: .phoneUDID)
         }
     }
     
     var language: String? {
         get {
-            return defaults.string(forKey: "Language")
+            return defaults.string(forKey: .language)
         }
         set {
-            defaults.set(newValue, forKey:"Language")
+            defaults.set(newValue, forKey: .language)
         }
     }
     
     var tag: String? {
         get {
-            return defaults.string(forKey: "Tag")
+            return defaults.string(forKey: .tag)
         }
         set {
-            defaults.set(newValue, forKey:"Tag")
+            defaults.set(newValue, forKey: .tag)
         }
     }
     
     var debugState: Int? {
         get {
-            return defaults.integer(forKey: "DebugCheckbox")
+            return defaults.integer(forKey: .debugState)
         }
         set {
-            defaults.setValue(newValue, forKey: "DebugCheckbox")
+            defaults.set(newValue, forKey: .debugState)
         }
     }
     
     var cucumberProfile: String? {
         get {
-            return defaults.string(forKey: "cucumberProfile")
+            return defaults.string(forKey: .cucumberProfile)
         }
         set {
-            defaults.set(newValue, forKey:"cucumberProfile")
+            defaults.set(newValue, forKey: .cucumberProfile)
         }
+    }
+}
+
+private extension UserDefaults {
+    func string(forKey defaultName: ApplicationStateHandler.Keys) -> String? {
+        return string(forKey: defaultName.rawValue)
+    }
+
+    func integer(forKey defaultName: ApplicationStateHandler.Keys) -> Int {
+        return integer(forKey: defaultName.rawValue)
+    }
+
+    func bool(forKey defaultName: ApplicationStateHandler.Keys) -> Bool {
+        return bool(forKey: defaultName.rawValue)
+    }
+
+    func url(forKey defaultName: ApplicationStateHandler.Keys) -> URL? {
+        return url(forKey: defaultName.rawValue)
+    }
+
+    func set(_ value: Any?, forKey defaultName: ApplicationStateHandler.Keys) {
+        set(value, forKey: defaultName.rawValue)
     }
 }
