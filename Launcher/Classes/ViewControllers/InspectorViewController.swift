5fe4251462181982bce81714150ae1fb12eb7eb5
Serghei Moret <serghei.moret@xing.com>
2017-11-08 14:45:11 +0100

    Move the init to viewDidLoad

--- Launcher/Classes/ViewControllers/InspectorViewController.swift
+++ Launcher/Classes/ViewControllers/InspectorViewController.swift
@@ -51,7 +51,6 @@ class InspectorViewController: NSViewController, NSTableViewDataSource {
     }
     
     override func viewDidAppear() {
-        textViewPrinter = TextViewPrinter(textView: outputText)
         outputText.backgroundColor = .darkAquamarine
         outputText.textColor = .lightGreen
         localizedTextField.textColor = .black
@@ -71,6 +70,7 @@ class InspectorViewController: NSViewController, NSTableViewDataSource {
         self.startDeviceButton.isEnabled = true
         self.gestureRecognizer.isEnabled = true
         self.cloneButton.isHidden = true
+        textViewPrinter = TextViewPrinter(textView: outputText)
     }
 
     @IBAction func doubleClickedItem(_ sender: NSOutlineView) {
