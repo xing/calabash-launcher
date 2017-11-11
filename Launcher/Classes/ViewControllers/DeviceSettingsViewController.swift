8c1bc5617e75cc76fac2c04663d72d6c95a68ebc
JoeSSS <joesatriani323@gmail.com>
2017-11-11 22:20:07 +0100

    elements rename

--- Launcher/Classes/ViewControllers/DeviceSettingsViewController.swift
+++ Launcher/Classes/ViewControllers/DeviceSettingsViewController.swift
@@ -3,24 +3,24 @@ import AppKit
 class DeviceSettingsViewController: NSViewController {
     let applicationStateHandler = ApplicationStateHandler()
 
-    @IBOutlet weak var textField: NSTextField!
-    @IBOutlet weak var bundleID: NSTextField!
+    @IBOutlet weak var deviceIPTextField: NSTextField!
+    @IBOutlet weak var bundleIDTextField: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if let deviceIP = applicationStateHandler.deviceIP {
-            textField.stringValue = deviceIP
+            deviceIPTextField.stringValue = deviceIP
         }
-        if let bundleIDdata = applicationStateHandler.bundleID {
-            bundleID.stringValue = bundleIDdata
+        if let bundleID = applicationStateHandler.bundleID {
+            bundleIDTextField.stringValue = bundleID
         }
     }
     
     @IBAction func changeDeviceIP(_ sender: Any) {
-        applicationStateHandler.deviceIP = textField.stringValue
+        applicationStateHandler.deviceIP = deviceIPTextField.stringValue
     }
     
     @IBAction func changeBundleID(_ sender: Any) {
-        applicationStateHandler.bundleID = bundleID.stringValue
+        applicationStateHandler.bundleID = bundleIDTextField.stringValue
     }
 }
