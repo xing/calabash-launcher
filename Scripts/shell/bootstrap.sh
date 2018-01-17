59c6f2a6bc95f7d31af5661616da5602ba573fa1
Martin Kim Dung-Pham <kim@elbedev.com>
2018-01-17 20:02:36 +0100

    Use bootstrap script in pre action of build phase This builds the dependencies before the actual build starts

--- Scripts/shell/bootstrap.sh
+++ Scripts/shell/bootstrap.sh
@@ -6,6 +6,12 @@
 #  Created by Martin Kim Dung-Pham on 14.01.18.
 #  Copyright © 2018 XING. All rights reserved.
 
-cd ./Core
-swift package update
-swift package generate-xcodeproj
+# walk into the directory managed by Swift Package Manager
+cd ${SOURCE_ROOT}/Core
+
+# Fetch new version if available and desired by the package manifest
+#swift package --disable-sandbox update
+
+# Build so the dependencies can be used in the Calabash Launcher (via Calabash Launcher Core)
+swift build --disable-sandbox
+
