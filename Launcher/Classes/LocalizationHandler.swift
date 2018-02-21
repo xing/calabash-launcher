b70fb35ce4c9b874877f7c74a9931a38eafb41a5
Serghei Moret <serghei.moret@xing.com>
2018-02-21 17:17:16 +0100

    Add support for json files

--- Launcher/Classes/LocalizationHandler.swift
+++ Launcher/Classes/LocalizationHandler.swift
@@ -5,18 +5,37 @@ class LocalizationHandler {
     let applicationStateHandler = ApplicationStateHandler()
     
     func getKeys(for string: String) -> [String] {
-        
         guard
             let value = parseResponse(string),
             let filePath = applicationStateHandler.filePath?.appendingPathComponent("build/Calabash.app/Frameworks/XNGLocalizedString.framework/en.lproj/Localizable.strings"),
             let stringsDict = NSDictionary(contentsOf: filePath) as? [String: String]
         else { return [] }
         
-        return stringsDict.keysForValue(value: value)
+        let resultingKeys = stringsDict.keysForValue(value: value)
+        
+        if resultingKeys.isEmpty {
+            if let path = applicationStateHandler.filePath?.appendingPathComponent("config/text_resources/content/en.json"),
+                let data = try? Data(contentsOf: path, options: .mappedIfSafe),
+                let jsonResult = try? JSONSerialization.jsonObject(with: data, options: .mutableLeaves) as? NSDictionary,
+                let enJSON = jsonResult?["en"] as? [String: Any]
+            {
+                let resultDictionary = filterDictionary(dictionary: enJSON)
+                return resultDictionary.keysForValue(value: value)
+            }
+        }
+        return resultingKeys
     }
     
     func parseResponse(_ response: String) -> String? {
         return RegexHandler().matches(for: "\\marked:'(.*?)\\\'", in: response).last
+    }
+    
+    func filterDictionary(dictionary: [String: Any]) -> [String: String] {
+        let letterCount: [String: String] = dictionary.reduce(into: [:]) { dict, item in
+            guard let value = item.value as? String else {return}
+            dict[item.key] = value
+        }
+        return letterCount
     }
     
 }
