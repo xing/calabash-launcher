a625448f2f23f29f3382902872df558d0b77a1d6
Bas Broek <bas@basbroek.nl>
2018-06-22 11:38:32 +0200

    Split the function into separate internal functions

--- Launcher/Classes/LocalizationHandler.swift
+++ Launcher/Classes/LocalizationHandler.swift
@@ -60,28 +60,35 @@ class LocalizationHandler {
     }
     
     func jsonKeys(for value: String) -> [String] {
-        var resultingKeys: [String] = []
-        
-        jsonFilePaths.compactMap { path -> NSDictionary? in
-            guard let path = applicationStateHandler.filePath?.appendingPathComponent(path),
-                let data = try? Data(contentsOf: path, options: .mappedIfSafe),
-                let jsonObject = try? JSONSerialization.jsonObject(with: data, options: .mutableLeaves),
-                let jsonDictionary = jsonObject as? NSDictionary else { return nil }
-            return jsonDictionary
-            }.forEach { jsonDictionary in
-                let jsonKeys = jsonDictionary.allKeys
-                
-                let jsonResults = jsonKeys.compactMap { key -> [String: Any]? in
-                    guard let _jsonDictionary = jsonDictionary[key] as? [String: Any] else { return nil }
-                    return _jsonDictionary
-                    }.flatMap { $0 }
-                
-                let resultDictionary = filterDictionary(jsonResults)
-                
-                resultingKeys.append(contentsOf: resultDictionary.keysForValue(value))
+        func jsonDictionaries(from filePaths: [String]) -> [NSDictionary] {
+            return filePaths.compactMap { path -> NSDictionary? in
+                guard let path = applicationStateHandler.filePath?.appendingPathComponent(path),
+                    let data = try? Data(contentsOf: path, options: .mappedIfSafe),
+                    let jsonObject = try? JSONSerialization.jsonObject(with: data, options: .mutableLeaves),
+                    let jsonDictionary = jsonObject as? NSDictionary else { return nil }
+                return jsonDictionary
+            }
         }
-        
-        return resultingKeys
+
+        func jsonKeys(from dictionaries: [NSDictionary]) -> [String] {
+            return dictionaries.map { jsonDictionary -> [String] in
+                let jsonKeys = jsonDictionary.allKeys
+                let jsonResultsDictionary = flattenValues(for: jsonDictionary, usingKeys: jsonKeys)
+
+                return filterDictionary(jsonResultsDictionary).keysForValue(value)
+            }.flatMap { $0 }
+        }
+
+        func flattenValues(for jsonDictionary: NSDictionary, usingKeys keys: [Any]) -> [String: Any] {
+            let tuples = keys.compactMap { key -> [String: Any]? in
+                guard let _jsonDictionary = jsonDictionary[key] as? [String: Any] else { return nil }
+                return _jsonDictionary
+            }.flatMap { $0 }
+            return Dictionary(uniqueKeysWithValues: tuples)
+        }
+
+        let dictionaries = jsonDictionaries(from: jsonFilePaths)
+        return jsonKeys(from: dictionaries)
     }
     
     static func allFiles(withFileExtension fileExtension: String) -> [String] {
